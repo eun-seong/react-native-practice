487ac7978ec7ae83d41f4c69d2812dffdc81521a
eunseonglee <eunseongofficial@gmail.com>
2021-09-15 14:24:45 +0900

    chore: styled-component 패키지 추가

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Container>
+        <Title>Hello</Title>
+      </Container>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +23,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+const Title = styled.Text`
+  color: tomato;
+  text-align: center;
+  font-size: 16px;
+`;
