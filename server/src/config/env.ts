2a8e7ee4c19fc511ddb0c0c6b13a4599a4d95f1a
eunseonglee <eunseongofficial@gmail.com>
2021-09-15 20:03:54 +0900

    [#1] chore: 로그인 인증 환경변수 설정

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -2,6 +2,8 @@ import loadEnvironment from './loadEnviroment';
 
 const env = {
   PORT: loadEnvironment('PORT'),
+  NAVER_CLIENT_ID: loadEnvironment('NAVER_CLIENT_ID'),
+  NAVER_CLIENT_SECRET: loadEnvironment('NAVER_CLIENT_SECRET'),
 };
 
 export default env;
