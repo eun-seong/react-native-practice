5426a11106ea79dfabb7068fdaf6d5eb34821c29
eunseonglee <eunseongofficial@gmail.com>
2021-09-15 23:33:03 +0900

    [#1] feature: 로그인 화면
    버튼 클릭 시 Naver OAuth2.0 앱 연동

--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import LoginButton from '../components/LoginButton';
+import SocialWebviewModal from '../components/SocialWebviewModal';
 
-export default function LoginScreen() {
+const LoginScreen = () => {
+  const [socialModalVisible, setSocialModalVisible] = useState<boolean>(false);
+  const [source, setSource] = useState<undefined | string>(undefined);
+
+  const closeSocialModal = () => {
+    setSocialModalVisible(!socialModalVisible);
+  };
+
+  const loginWithSocial = (social: string) => {
+    setSocialModalVisible(!socialModalVisible);
+    setSource(
+      `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=Idvq5aCHJ0BohQIPOban&state=STATE_STRING&redirect_uri=http://localhost:3000/api/auth/naver`,
+    );
+    // setSource(`http://localhost:3000/auth/${social}`);
+  };
+
   return (
     <Wrapper>
-      <LoginButton title={'NAVER'} />
+      {source && (
+        <SocialWebviewModal
+          visible={socialModalVisible}
+          source={source}
+          closeSocialModal={closeSocialModal}
+        />
+      )}
+      <LoginButton title={'naver'} onPress={() => loginWithSocial('naver')} />
     </Wrapper>
   );
-}
+};
+
+export default LoginScreen;
 
 const Wrapper = styled.View`
   flex: 1;
