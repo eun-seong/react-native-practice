7967b9a080d678ca98495272ad94a76f4c6acadf
eunseonglee <eunseongofficial@gmail.com>
2021-09-23 01:45:51 +0900

    chore: env 파일에 jwt 관련 추가

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -2,6 +2,8 @@ import loadEnvironment from './loadEnviroment';
 
 const env = {
   PORT: loadEnvironment('PORT'),
+  JWT_SECRET: loadEnvironment('JWT_SECRET'),
+  JWT_EXPIRES_IN: loadEnvironment('JWT_EXPIRES_IN'),
   NAVER_CLIENT_ID: loadEnvironment('NAVER_CLIENT_ID'),
   NAVER_CLIENT_SECRET: loadEnvironment('NAVER_CLIENT_SECRET'),
 };
