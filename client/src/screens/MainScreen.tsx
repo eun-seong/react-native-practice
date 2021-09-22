ac70541e0dcd459a280d23b9f375807c79f84097
eunseonglee <eunseongofficial@gmail.com>
2021-09-23 01:44:04 +0900

    feature: Main 페이지 구현

---
+++ client/src/screens/MainScreen.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const MainScreen = () => {
+  return (
+    <Wrapper>
+      <Text>MAin</Text>
+    </Wrapper>
+  );
+};
+
+export default MainScreen;
+
+const Wrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
