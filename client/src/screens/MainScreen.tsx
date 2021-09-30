3331c4cc3c75bfaba3f14b1ef488f27457bef58a
eunseonglee <eunseongofficial@gmail.com>
2021-09-30 22:47:21 +0900

    feature: 구글 맵 관련 설정

--- client/src/screens/MainScreen.tsx
+++ client/src/screens/MainScreen.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import styled from 'styled-components/native';
 
 const MainScreen = () => {
   return (
     <Wrapper>
+      <Text>MAin</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
       <Text>MAin</Text>
     </Wrapper>
   );
@@ -16,4 +28,16 @@ const Wrapper = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+`;
+
+const MapWrapper = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Map = styled(MapView)`
+  width: 100%;
+  flex: 1;
 `;
