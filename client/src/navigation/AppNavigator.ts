4ed1e628c8660247563da51a46954ce04c67a8d1
eunseonglee <eunseongofficial@gmail.com>
2021-09-30 22:47:04 +0900

    feature: 메인페이지 대신 탭바 있는 네비게이터로 변경

--- client/src/navigation/AppNavigator.ts
+++ client/src/navigation/AppNavigator.ts
@@ -1,12 +1,12 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import MainScreen from '../screens/MainScreen';
 import LoginScreen from '../screens/LoginScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+import TabNavigator from './TabNavigator';
 
-const AppStack = createStackNavigator({ Main: MainScreen });
-const AuthStack = createStackNavigator({ SignIn: LoginScreen });
+const AppStack = createStackNavigator({ App: { screen: TabNavigator } });
+const AuthStack = createStackNavigator({ SignIn: { screen: LoginScreen } });
 
 export default createAppContainer(
   createSwitchNavigator(
